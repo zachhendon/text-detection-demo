f75a7f56b5d7ee209724c6e79d81ef725c3d9f88
ZachHendon <zhendon@wisc.edu>
2024-10-21 13:58:34 -0500

    Finalized websocket and added data loading, postprocessing, and serialization

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
 import modal
-import json
 import io
-import pickle
 import base64
 import time
-from fastapi.responses import StreamingResponse, HTMLResponse
-from fastapi import FastAPI, WebSocket
 
 
 app = modal.App("text-detection-demo")
-# fast_app = FastAPI()
 model_volume = modal.Volume.from_name("model-export")
 
 pytorch_image = modal.Image.from_registry("nvcr.io/nvidia/pytorch:24.09-py3")
-pytorch_image.pip_install("fastapi", "websockets")
+pytorch_image.pip_install("pillow")
 with pytorch_image.imports():
     import torch
     import torchvision
     import torch_tensorrt
+    import numpy as np
+websocket_image = modal.Image.debian_slim().pip_install("pillow")
+with websocket_image.imports():
+    from PIL import Image
 
 
 @app.cls(image=pytorch_image, volumes={"/models": model_volume}, gpu="L4")
@@ -27,43 +26,57 @@ class Model:
         import torch
 
         self.model = torch.export.load("/models/model.ep").module()
+        self.threshold = 0.55
+        self.blur_kernel = (
+            torch.tensor(
+                [[[[1.0, 2.0, 1.0], [2.0, 4.0, 2.0], [1.0, 2.0, 1.0]]]], device="cuda"
+            )
+            / 16
+        )
 
     @modal.method()
     def forward(self, x):
-        s = time.time()
-        inp = torch.from_numpy(x).to(dtype=torch.float32, device="cuda")
-        # inp = torch.randn(1, 3, 640, 1140).cuda()
-        print(time.time() - s)
-        out = self.model(inp)
-        print(time.time() - s)
-        print()
-        return out.sum().item()
+        input_image = Image.open(io.BytesIO(base64.b64decode(x)))
+        input = torchvision.transforms.PILToTensor()(input_image)
+        input = (input / 255).to(dtype=torch.float32, device="cuda")
+        print(input.shape)
+        input = torchvision.transforms.Resize((640, 1140))(input)
+        print(input.shape)
+        input = input.unsqueeze(0)
+        out = self.model(input)
 
-    # @modal.method()
-    # def inference(self):
-    #     out = self.forward.remote(None)
-        # image = torchvision.transforms.ToPILImage()(out)
-        # return_image = io.BytesIO()
-        # image.save(return_image, "JPEG")
-        # return_image.seek(0)
-        # return StreamingResponse(content=return_image, media_type="image/jpeg")
+        # process and serialize
+        blurred = torch.nn.functional.conv2d(
+            out.unsqueeze(0),
+            self.blur_kernel,
+            stride=1,
+            padding=1,
+        ).squeeze(0)
+        binary = (blurred > self.threshold).to(dtype=torch.uint8) * 255
+        binary = torchvision.transforms.Resize((640, 480))(binary).permute(0, 2, 1)
+        binary_image = torchvision.transforms.ToPILImage()(binary)
+        buffer = io.BytesIO()
+        binary_image.save(buffer, format="JPEG")
+        binary_base64 = base64.b64encode(buffer.getvalue())
+        return binary_base64
 
-@app.function()
+
+@app.function(image=websocket_image)
 @modal.asgi_app()
 def endpoint():
     from fastapi import FastAPI, WebSocket
     import numpy as np
-    
+
     app = FastAPI()
 
     @app.websocket("/ws")
     async def websocket_handler(websocket: WebSocket) -> None:
         await websocket.accept()
-        await websocket.send_text("WORKING")
         while True:
             data = await websocket.receive_text()
+            s = time.time()
+            out = Model().forward.remote(data)
+            print(time.time() - s)
+            await websocket.send_text(f"{out}")
 
-            x = np.random.randn(1, 3, 640, 1140)
-            out = Model().forward.remote(x)
-            await websocket.send_text(f"{data}: {out}")
     return app
