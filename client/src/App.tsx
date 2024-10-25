d5b786f122a5472f2efa3bb61f47e5bf69688144
Zach Hendon <zachendo3@gmail.com>
2024-10-25 17:31:33 -0500

    Polished UI and connection process

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,29 @@
 import { useRef, useEffect, useState } from "react";
-import useWebSocket, { ReadyState } from "react-use-websocket";
+import useWebSocket from "react-use-websocket";
 import "./App.css";
 
+interface Dimensions {
+  width: number;
+  height: number;
+}
+
 function App() {
+  const [dimensions] = useState<Dimensions>({
+    width: 640,
+    height: 480,
+  });
   const videoRef = useRef<HTMLVideoElement>(null);
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   const canvasCtxRef = useRef<CanvasRenderingContext2D | null>(null);
-  const imageRef = useRef<HTMLImageElement>(new Image(640, 480));
   const maskRef = useRef<HTMLCanvasElement | null>(null);
   const maskCtxRef = useRef<CanvasRenderingContext2D | null>(null);
+  const imageRef = useRef<HTMLImageElement>(
+    new Image(dimensions.width, dimensions.height)
+  );
   const frameIdRef = useRef<number | null>(null);
   const previousTimeRef = useRef<number>(0);
   const intervalRef = useRef<number>(1000 / 30);
+  const [isConnected, setIsConnected] = useState<boolean>(false);
 
   const socketUrl =
     "wss://zachhendon--text-detection-demo-endpoint.modal.run/ws";
@@ -28,7 +40,9 @@ function App() {
   };
 
   const updateMaskCanvas = (event: WebSocketEventMap["message"]): void => {
-    if (!maskRef.current ||!imageRef.current) return;
+    setIsConnected(true);
+
+    if (!maskRef.current || !imageRef.current) return;
 
     imageRef.current.src = `data:image/jpg;base64,${event.data.slice(
       2,
@@ -69,13 +83,19 @@ function App() {
     }, "image/jpeg");
   };
 
-  const { sendMessage, readyState } = useWebSocket(socketUrl, {
+  const { sendMessage } = useWebSocket(socketUrl, {
     share: true,
     onOpen: (): void => {
       if (frameIdRef.current) {
-        cancelAnimationFrame(frameIdRef.current);``
+        cancelAnimationFrame(frameIdRef.current);
       }
       frameIdRef.current = requestAnimationFrame(animate);
+    },
+    onClose: (): void => {
+      if (frameIdRef.current) {
+        cancelAnimationFrame(frameIdRef.current);
+      }
+      setIsConnected(false);
     },
     onMessage: updateMaskCanvas,
   });
@@ -84,46 +104,59 @@ function App() {
     const getCamera = async (): Promise<void> => {
       try {
         const stream: MediaStream = await navigator.mediaDevices.getUserMedia({
-          video: { width: 640, height: 480 },
+          video: { width: dimensions.width, height: dimensions.height },
         });
-        stream.getVideoTracks();
         if (videoRef.current) {
           videoRef.current.srcObject = stream;
         }
-        if (canvasRef.current && videoRef.current) {
+        if (canvasRef.current) {
+          canvasRef.current.width = dimensions.width;
+          canvasRef.current.height = dimensions.height;
           canvasCtxRef.current = canvasRef.current.getContext("2d");
         }
         if (maskRef.current) {
-          maskRef.current.width = 640;
-          maskRef.current.height = 480;
+          maskRef.current.width = dimensions.width;
+          maskRef.current.height = dimensions.height;
           maskCtxRef.current = maskRef.current.getContext("2d");
         }
-      } catch (vent) {
-        console.log("Error: no camera found on this device");
+      } catch {
+        alert("Error: no camera found on this device");
+        return;
       }
     };
     getCamera();
   }, []);
 
-  const connectionStatus = {
-    [ReadyState.CONNECTING]: "Connecting",
-    [ReadyState.OPEN]: "Open",
-    [ReadyState.CLOSING]: "Closing",
-    [ReadyState.CLOSED]: "Closed",
-    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
-  }[readyState];
-
   return (
     <div>
-      <p>The WebSocket is currently {connectionStatus}</p>
+      <p>
+        {!isConnected
+          ? "Currently connecting to server. Estimated startup time: 20-30 seconds."
+          : `Detecting text at ${Math.round(1000 / intervalRef.current)} fps`}
+      </p>
       <video
         ref={videoRef}
         autoPlay
-        style={{ width: "640px", height: "480px" }}
+        style={{
+          width: `${dimensions.width}px`,
+          height: `${dimensions.height}px`,
+        }}
       ></video>
-      <button onClick={captureFrame}>CLICK</button>
-      <canvas ref={canvasRef} style={{ width: "640px", height: "480px", display: "none" }} />
-      <canvas ref={maskRef} style={{ width: "640px", height: "480px" }} />
+      <canvas
+        ref={canvasRef}
+        style={{
+          width: `${dimensions.width}px`,
+          height: `${dimensions.height}px`,
+          display: "none",
+        }}
+      />
+      <canvas
+        ref={maskRef}
+        style={{
+          width: `${dimensions.width}px`,
+          height: `${dimensions.height}px`,
+        }}
+      />
     </div>
   );
 }
